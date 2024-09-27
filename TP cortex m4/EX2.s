c8432ea1f4902e15016851dfbf2f603186b8bd60
Said Agouzal <said.agouzal@telecom-paris.fr>
2024-09-27 15:24:24 +0000

    Update 3 files
    - /TP cortex m4/EX1.s
    - /TP cortex m4/EX2.s
    - /TP cortex m4/EX3.s

---
+++ TP cortex m4/EX2.s
@@ -0,0 +1,34 @@
+.syntax unified
+.arch armv7-m
+.cpu cortex-m4
+
+.thumb
+
+.global _start
+_start:
+ldr r0,= 0xdeadbeef
+ldr r1,= 0x10000000
+mov r2,#0
+mov r3,#64
+loop:
+SUBS r5,r3,r2
+beq  copy
+str r0,[r1]
+add r1,r1,#4
+add r2,r2,#1
+b loop
+
+copy:
+ldr r1,= 0x10000000
+ldr r3,= 0x100
+ldr r0,=0x10000300
+loop2: 
+ldr r9,[r1]
+str r9,[r0]
+add r1,r1,#4
+add r0,r0,#4
+SUBS r3,r3,#-4
+bnq loop2
+
+end : 
+b end 
---
+++ TP cortex m4/EX3.s
@@ -0,0 +1,54 @@
+.syntax unified
+.arch armv7-m
+.cpu cortex-m4
+
+.thumb
+
+.global _start
+_start:
+/*premier remplisssage et copage*/
+ldr r0,=0x10000000
+ldr r1,= 0xdeadbeef
+ldr r2,= 256
+bl etape1
+
+ldr r0, =0x10000300
+ldr r1, =0x10000000
+ldr r2, =256
+bl etape2 
+
+/* deuxiemme remplissage et copage*/
+
+ldr r0,=0x10003000
+ldr r1,= 0xcafecafe
+ldr r2,= 64
+bl etape1
+
+ldr r0, =0x10006000
+ldr r1, =0x10003000
+ldr r2, =32
+bl etape2 
+
+
+/*remplissage*/
+
+etape1:
+subs r2,r2,#-4
+beq  endEtape1
+str r1,[r0]
+add r0,r0,#4
+b etape1
+endEtape1:
+bx lr
+
+/*copage*/
+
+etape2: 
+ldr r9,[r1]
+str r9,[r0]
+add r1,r1,#4
+add r0,r0,#4
+SUBS r2,r2,#-4
+bnq etape2
+bx lr
+
